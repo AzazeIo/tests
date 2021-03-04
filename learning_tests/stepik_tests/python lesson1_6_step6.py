0fb0ea25f810bf7683953975f41f7fc6ba2980d7
GOLOVKINA USTINYA <sakura.ustinya@yandex.ru>
2021-03-04 17:42:36 +0300

    install pyperclip package, edit pipfile, edit stepick-course tests(delete comments lines)

--- learning_tests/stepik_tests/python lesson1_6_step6.py
+++ learning_tests/stepik_tests/python lesson1_6_step6.py
@@ -10,26 +10,14 @@ try:
     add_button = browser.find_element_by_css_selector(".add")
     add_button.click()
 
-# открываем страницу второго товара
     browser.get("https://fake-shop.com/book2.html")
-
-# добавляем товар в корзину
     add_button = browser.find_element_by_css_selector(".add")
     add_button.click()
-
-# тестовый сценарий
-# открываем корзину
     browser.get("https://fake-shop.com/basket.html")
-
-# ищем все добавленные товары
     goods = browser.find_elements_by_css_selector(".good")
-
-# проверяем, что количество товаров равно 2
     assert len(goods) == 2
 
 finally:
-    # успеваем скопировать код за 30 секунд
     time.sleep(5)
-    # закрываем браузер после всех манипуляций
     browser.quit()
 
